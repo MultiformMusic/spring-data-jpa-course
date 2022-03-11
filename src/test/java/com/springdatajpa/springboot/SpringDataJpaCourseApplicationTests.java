5b6e0d6b30c584511d9e02ab3cc5f1a43d78d0fb
Michel Dio <michel.dio33@gmail.com>
2022-03-11 09:26:03 +0100

    no test

--- src/test/java/com/springdatajpa/springboot/SpringDataJpaCourseApplicationTests.java
+++ src/test/java/com/springdatajpa/springboot/SpringDataJpaCourseApplicationTests.java
@@ -1,12 +1,11 @@
 package com.springdatajpa.springboot;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDataJpaCourseApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
