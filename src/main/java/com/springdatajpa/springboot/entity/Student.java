88878cbd918ef2b74bbd8dc6ebe5bcb2f3da6cba
Michel Dio <michel.dio33@gmail.com>
2022-03-11 09:23:48 +0100

    test

--- src/main/java/com/springdatajpa/springboot/entity/Student.java
+++ src/main/java/com/springdatajpa/springboot/entity/Student.java
@@ -19,7 +19,6 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String firstName;
     private String lastName;
     private String email;
