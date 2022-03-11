5aabfa0987e91d51e73d883cc500fd660e1653a1
Michel Dio <michel.dio33@gmail.com>
2022-03-11 09:01:18 +0100

    initial commit

---
+++ src/main/java/com/springdatajpa/springboot/entity/Student.java
@@ -0,0 +1,28 @@
+package com.springdatajpa.springboot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDateTime dateCreated;
+    private LocalDateTime lastUpdated;
+}
